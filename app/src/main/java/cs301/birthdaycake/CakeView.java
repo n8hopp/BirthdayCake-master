571228553854b5785027d0cc3fb4f1df11eb0d96
n8hopp <hopper26@up.edu>
2022-09-01 16:36:50 -0700

    Adding 'frosting' switch & adding second candle at 2/3 point

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -119,8 +119,9 @@ public class CakeView extends SurfaceView {
         //Then a second cake layer
         canvas.drawRect(cakeLeft, top, cakeLeft + cakeWidth, bottom, cakePaint);
 
-        //Now a candle in the center
-        drawCandle(canvas, cakeLeft + cakeWidth/2 - candleWidth/2, cakeTop);
+        //Now two candles in the center
+        drawCandle(canvas, cakeLeft + cakeWidth/3 - candleWidth/2, cakeTop);
+        drawCandle(canvas, cakeLeft + cakeWidth/3*2 - candleWidth/2, cakeTop);
 
     }//onDraw
 
